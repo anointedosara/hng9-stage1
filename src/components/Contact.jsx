cc8964fa9ee7c9f6b43d2376717a92a931edc5c1
anointed osara <anointedosara@gmail.com>
2022-11-03 13:27:38 +0530

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Contact() {
+  const [data, setData] = useState({
+    fName: '',
+    lName: '',
+    email: '',
+    message: '',
+    agree: false
+})
   return (
-    <div>
+    <div className='contact'>
       <h1>Contact Me</h1>
       <p>Hi there, contact me to ask me about anything you have in mind.</p>
-      <form>
-        <div>
-          <div>
-            <label for="fname">First name</label><br />
-            <input type="text" id="fname" name="fname" value="" placeholder='Enter your first name' />
-          </div>
-          <div>
-            <label for="lname">Last name</label><br />
-            <input type="text" id="lname" name="lname" value="" placeholder='Enter your last name' />
-          </div>
+      <form className='contact-form'>
+        <div className='contact-names'>
+          <label htmlFor="fname">First name<br />
+            <input type="text" id="fname" name="fname" value={data?.fName} onChange={(e) => setData({
+                        ...data,
+                        fName: e.target.value
+                    })} placeholder='Enter your first name' required />
+          </label>
+          <label htmlFor="lname" className='lname'>Last name<br />
+            <input type="text" id="lname" name="lname" value={data?.lName} onChange={(e) => setData({
+                        ...data,
+                        lName: e.target.value
+                    })} placeholder='Enter your last name' required />
+          </label>
         </div>
 
-        <label for="email">Email</label><br />
-        <input type="email" id="email" name="email" value="" placeholder='yourname@email.com' /><br />
-        <label for="message">Message</label><br />
-        <textarea name="message" id="message" value="" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
-        <input type="checkbox" id="checkbox" /><label for="checkbox">You agree to providing your data to Anointed Osara who may contact you.</label>
+        <label htmlFor="email">Email<br />
+          <input type="email" id="email" name="email" value={data?.email} onChange={(e) => setData({
+                        ...data,
+                        email: e.target.value
+                    })} placeholder='yourname@email.com' required /><br />
+        </label>
+        <label htmlFor="message">Message<br />
+          <textarea name="message" id="message" value={data?.message} onChange={(e) => setData({
+                        ...data,
+                        message: e.target.value
+                    })} placeholder="Send me a message and I'll reply you as soon as possible..." required></textarea>
+        </label>
+        <label htmlFor="checkbox" className='container'>
+          <input type="checkbox" id="checkbox" checked={data?.agree} onChange={(e) => setData({
+                        ...data,
+                        agree: !data?.agree
+                    })} required />You agree to providing your data to OsaraAnointed who may contact you.
+          <span className="checkmark"></span>
+        </label><br />
         <button id='btn__submit'>Send message</button>
       </form>
     </div>
