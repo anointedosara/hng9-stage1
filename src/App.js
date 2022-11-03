d71ba99f0ba1df1c37800966a16aa7863174d2bd
anointed osara <anointedosara@gmail.com>
2022-11-03 14:59:00 +0530

    fix: A bug fix

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-     <Route path='/' exact component={Home} />
-     <Route path='/contact' component={Contact} />
+      <Route path='/' exact component={Home} />
+      <Route path='/contact' component={Contact} />
      <Footer />
     </div>
   );
